96ed6a07848ba8a473a7ecc36aa84f8f12882020
oanh dc23v7x314 <thuyoanh081196@gmail.com>
2025-04-10 20:28:44 +0700

    Cai dat express chay thong bao xin chao

---
+++ server.js
@@ -0,0 +1,7 @@
+const app = require("./app");
+const config = require("./app/config");
+// Bắt đầu server
+const PORT = config.app.port || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
